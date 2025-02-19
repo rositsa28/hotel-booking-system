3c9f08d88e85422a00d4df33fdc23e8ea305935f
rositsa28 <156697740+rositsa28@users.noreply.github.com>
2025-02-19 21:30:39 +0200

    initial commit

---
+++ src/test/java/bg/softuni/hotelbookingsystem/HotelBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.hotelbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
